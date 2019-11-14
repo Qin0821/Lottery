60ff55fda48e7fe1d64d3dc739f0a60bb8e746e3
Qin <liuqin0821@gmail.com>
2019-11-14 21:08:00 +0800

    完成打包以及项目的初始化
    描述：
    思路：
    测试：

--- main.js
+++ main.js
@@ -4,16 +4,25 @@
  * @license Copyright (c) 2019 那镁克
  */
 // 引入electron并创建一个Browserwindow
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
 const url = require('url');
 
 // 保持window对象的全局引用,避免JavaScript对象被垃圾回收时,窗口被自动关闭.
-let mainWindow;
+let showWindow;
 
 function createWindow () {
 //创建浏览器窗口,宽高自定义具体大小你开心就好
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  showWindow = new BrowserWindow({
+    minWidth: 800,
+    minHeight: 600,
+    // fullscreenable: true,
+    // fullscreen: true,
+    width: screen.getPrimaryDisplay().workAreaSize.width,
+    height: screen.getPrimaryDisplay().workAreaSize.height,
+    titleBarStyle: 'default',
+    backgroundColor: '#000000',
+  });
 
   /*
    * 加载应用-----  electron-quick-start中默认的加载入口
@@ -24,19 +33,19 @@ function createWindow () {
     }))
   */
   // 加载应用----适用于 react 项目
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, './build/index.html'),
+  showWindow.loadURL(url.format({
+    pathname: path.join(`${__dirname}/src/index.html`),
     protocol: 'file:',
     slashes: true
   }));
 
   // 打开开发者工具，默认不打开
-  // mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools();
 
   // 关闭window时触发下列事件.
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+  showWindow.on('closed', function () {
+    showWindow = null;
+  });
 }
 
 // 当 Electron 完成初始化并准备创建浏览器窗口时调用此方法
@@ -46,14 +55,14 @@ app.on('ready', createWindow);
 app.on('window-all-closed', function () {
   // macOS中除非用户按下 `Cmd + Q` 显式退出,否则应用与菜单栏始终处于活动状态.
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 });
 
 app.on('activate', function () {
   // macOS中点击Dock图标时没有已打开的其余应用窗口时,则通常在应用中重建一个窗口
-  if (mainWindow === null) {
-    createWindow()
+  if (showWindow === null) {
+    createWindow();
   }
 });
 
