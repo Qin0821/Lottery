b308169e2cc7861f2a7d8a05bf1d5926c7693f0c
Qin <liuqin0821@gmail.com>
2019-12-15 16:59:44 +0800

    更新中奖名单布局
    描述：
    思路：
    测试：

--- src/js/resultKeyDown.js
+++ src/js/resultKeyDown.js
@@ -12,43 +12,26 @@ function keyDown (e) {
   var keyName = String.fromCharCode(currKey);
 
   console.log('按键码: ' + currKey + ' 字符: ' + keyName);
-  if (currKey === 48) {
-    // 0 -> 重置
-    reset();
-  } else if (currKey === 49) {
+  if (currKey === 49) {
     // 1 -> 一等奖
-    handleKeyup(5, 1);
+    onClick(1);
   } else if (currKey === 50) {
     // 2 -> 二等奖
-    handleKeyup(5, 2);
+    onClick(2);
   } else if (currKey === 51) {
     // 3 -> 三等奖
-    handleKeyup(10, 3);
+    onClick(3);
   } else if (currKey === 52) {
     // 4 -> 四等奖
-    handleKeyup(25, 4);
+    onClick(4);
   } else if (currKey === 53) {
     // 5 -> 五等奖
-    handleKeyup(30, 5);
+    onClick(5);
   } else if (currKey === 54) {
     // 6 -> 特等奖
-    handleKeyup(1, 0);
-  }
-    // else if (currKey > 48 && currKey < 58) {
-    //   // 1 - 9 设置抽奖人数
-    //   handleKeyup(keyName);
-  // }
-  else if (currKey === 13) {
-    // enter 开始/停止抽奖
-    toggle();
-  } else if (currKey === 27) {
-    // esc 关闭抽奖框
-    $('#result').css('display', 'none');
-    $('#main').removeClass('mask');
-  } else if (currKey === 187) {
-    window.location.href = 'result.html';
+    onClick(0);
   } else if (currKey === 189) {
-    // window.location.href = 'index.html';
+    window.location.href = 'index.html';
   }
 }
 
--- src/js/test.js
+++ src/js/test.js
@@ -8,22 +8,62 @@
   console.log(localStorage);
   console.log(choosed);
   new Vue({
-    el: '#tools',
+    el: '#main',
     data: {
-      selected: 6,
-      running: false,
-      btns: [
-        6, 4, 3, 2, 1
-      ],
-      level: 5,
+      results: [],
+      result: {
+        data: '',
+      },
+      level: 1,
     },
     mounted () {
       window.onClick = this.onClick;
+
+      let vm = this;
+      let locals = window.localStorage;
+      console.log(locals);
+      let str_results = [];
+      // 遍历(排除choosed)
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'choosed') {
+          str_results.push(localStorage.getItem(localStorage.key(i)));
+        }
+      }
+      // 分割每个string, 并将</br>换成空格
+      let results = [];
+      for (let nameList of str_results) {
+        let temp = JSON.parse(nameList);
+        const data = temp.data.map(item => {return item.replace('<br/>', ' ');});
+        const a = {
+          level: temp.level,
+          data: data,
+        };
+        if (temp.level === this.level) {
+          this.result = a;
+        }
+        results.push(a);
+      }
+
+      // 根据长度进行排序，名单短的放在前面
+      results = results.sort((x, y) => {
+        return x.level - y.level;
+      });
+      console.log(results + 'bbb');
+      this.results = results;
     },
     methods: {
-      onClick: function (num, level) {
-        console.log(num, level)
-        this.selected = num;
+      onClick: function (level) {
+        console.log(level);
+        let r = {
+          level: level
+        };
+        for (let result of this.results) {
+          if (result.level === level) {
+            r = result;
+          }
+        }
+        this.result = r
+        console.log(this.result)
         this.level = level;
         window.level = level;
       },
